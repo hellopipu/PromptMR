8d337c2bcb498a04f1b46a090cd22a809b8e60a1
hellopipu <2991874920@qq.com>
2023-10-31 03:40:13 -0400

    update run script

--- promptmr_examples/cmrxrecon/run.sh
+++ promptmr_examples/cmrxrecon/run.sh
@@ -1,8 +1,7 @@
 # 0. prepare dataset
 python prepare_h5py_dataset_for_training.py \
---fully_cine_matlab_folder /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil/Cine/TrainingSet/FullSample \
---fully_mapping_matlab_folder /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil/Mapping/TrainingSet/FullSample \
---save_folder_name h5_FullSample
+--data_path /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil \
+--h5py_folder h5_FullSample
 
 # 1. training command
 ## train promptmr-12cascades model
@@ -11,9 +10,11 @@ CUDA_VISIBLE_DEVICES=0,1 python train_promptmr_cmrxrecon.py \
 --accelerations 4 8 10 \
 --challenge multicoil \
 --mask_type equispaced_fixed \
---data_path /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil/Cine/TrainingSet/h5_FullSample \
+--data_path /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil \
+--h5py_folder h5_FullSample \
 --combine_train_val True \
 --exp_name promptmr_trainval \
+--num_cascades 12 \
 --use_checkpoint
 
 ## train promptmr-16cascades model
@@ -22,7 +23,8 @@ CUDA_VISIBLE_DEVICES=0,1 python train_promptmr_cmrxrecon.py \
 --accelerations 4 8 10 \
 --challenge multicoil \
 --mask_type equispaced_fixed \
---data_path /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil/Cine/TrainingSet/h5_FullSample \
+--data_path /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil \
+--h5py_folder h5_FullSample \
 --combine_train_val True \
 --exp_name promptmr_16_cascades_trainval \
 --num_cascades 16 \
@@ -33,11 +35,11 @@ CUDA_VISIBLE_DEVICES=0,1 python train_promptmr_cmrxrecon.py \
 
 ## use pretrained promptmr-12cascades model 
 CUDA_VISIBLE_DEVICES=1  python run_pretrained_promptmr_cmrxrecon_inference_from_matlab_data.py \
---input /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData \
+--input /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil \
 --output /research/cbim/vast/bx64/PycharmProjects/cmr_challenge_results/reproduce_promptmr_12_cascades_cmrxrecon \
 --model_path pretrained_models/promptmr-12cascades-epoch=11-step=258576.ckpt \
 --evaluate_set ValidationSet \
---stage 1 \
+--task Both \
 --batch_size 4 \
 --num_works 2 \
 --center_crop \
@@ -45,11 +47,11 @@ CUDA_VISIBLE_DEVICES=1  python run_pretrained_promptmr_cmrxrecon_inference_from_
 
 ## use pretrained promptmr-16cascades model 
 CUDA_VISIBLE_DEVICES=1  python run_pretrained_promptmr_cmrxrecon_inference_from_matlab_data.py \
---input /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData \
+--input /research/cbim/datasets/fastMRI/CMRxRecon/MICCAIChallenge2023/ChallengeData/MultiCoil \
 --output /research/cbim/vast/bx64/PycharmProjects/cmr_challenge_results/reproduce_promptmr_16cascades_cmrxrecon \
 --model_path pretrained_models/promptmr-16cascades-epoch=11-step=258576.ckpt \
 --evaluate_set ValidationSet \
---stage 1 \
+--task Both \
 --batch_size 4 \
 --num_works 2 \
 --center_crop \
